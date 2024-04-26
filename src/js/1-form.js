520243753aecf5f37373e3e7707617ec3da2f828
oko1o <okolomanov@gmail.com>
2024-04-26 13:18:36 -0400

    hw09 v1.0

---
+++ src/js/1-form.js
@@ -0,0 +1,41 @@
+export const addFont = document.head.insertAdjacentHTML(
+  'beforeend',
+  '<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>'
+);
+
+const formEl = document.querySelector('.feedback-form');
+
+export const inputEvent = formEl.addEventListener('input', event => {
+  const formData = {
+    email: formEl.elements.email.value.trim(),
+    message: formEl.elements.message.value.trim(),
+  };
+  console.log(formData);
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+export const checkStorage = window.addEventListener('load', () => {
+  const storedData = localStorage.getItem('feedback-form-state');
+  if (storedData) {
+    const { email, message } = JSON.parse(storedData);
+    formEl.elements.email.value = email;
+    formEl.elements.message.value = message;
+  }
+});
+
+export const submitForm = formEl.addEventListener('submit', event => {
+  const emailValue = formEl.elements.email.value.trim();
+  if (emailValue.includes('@') && emailValue.includes('.')) {
+    event.preventDefault();
+    const formData = {
+      email: formEl.elements.email.value.trim(),
+      message: formEl.elements.message.value.trim(),
+    };
+    localStorage.removeItem('feedback-form-state');
+    formEl.reset();
+    console.log(formData);
+  } else {
+    event.preventDefault();
+    alert('Type valid email');
+  }
+});
---
+++ src/main.js
@@ -0,0 +1,2 @@
+import { galleryHtml, lightBox } from "./js/1-gallery.js";
+import { addFont, inputEvent, checkStorage, submitForm } from "./js/1-form.js";
